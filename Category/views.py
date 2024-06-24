69f243e9717274c9528e2c326aa953ae1b678542
vini <101150191+vini-tn@users.noreply.github.com>
2024-06-24 19:37:24 +1200

    Working Search and Category functions

--- Category/views.py
+++ Category/views.py
@@ -10,7 +10,25 @@ def home(request):
 class ArticleListView(ListView):
     model = Article
     template_name = 'Category/articles/articles.html'
+
     # login_url = 'accounts/login/'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        category = self.request.GET.get('category', 'all')
+
+        if query:
+            queryset = queryset.filter(name__icontains=query)
+        if category and category != 'all':
+            queryset = queryset.filter(category__name__iexact=category)
+        
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_category'] = self.request.GET.get('category', 'all')
+        return context
 
 class ArticleDetailView (DetailView):
     model = Article

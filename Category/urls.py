557fe739ca44fd01c7b0012859b17242ac3344c6
nlkx <nicolle.lim@gmail.com>
2024-06-25 12:32:15 +1200

    changed styling for navbars and buttons

--- Category/urls.py
+++ Category/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name=""),
+    path('', views.HomeView.as_view(), name='home'),
     path('articles', views.ArticleListView.as_view(), name='articles'),
     path('articles/<int:pk>', views.ArticleDetailView.as_view(), name='articles-details'),
 ]
